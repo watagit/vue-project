cad75effb1fd327db07e1eee3273878a31e1d601
Wataru Ono <wtr0414@gmail.com>
2020-04-29 07:50:30 +0900

    Begin Conditional Rendering

--- bindings/index.js
+++ bindings/index.js
@@ -1,17 +1,9 @@
-Vue.component('my-component', {
-  template: '<div><button v-on:click="change_function">{{ button_text }}</button></div>',
-  data: function () {
-    return {
-      button_text: 'お前を芸術品に仕立ててや・・・'
-    }
-  },
-  methods: {
-    change_function: function () {
-      this.button_text = '仕立て上げてやんだよ'
-    }
-  },
-})
-
 new Vue({
-  el: '#example'
+  el: '#app',
+  data: {
+    styleObject: {
+      color: 'red',
+      fontSize: '13px'
+    }
+  }
 })
