e6f8301046e58f3a103085ead25436872e743198
Wataru Ono <wtr0414@gmail.com>
2020-04-29 17:42:18 +0900

    Begin Components Basics

---
+++ form_input_bindings/index.js
@@ -0,0 +1,34 @@
+new Vue({
+  el: '#example-1',
+  data: {
+    message: ''
+  }
+})
+
+new Vue({
+  el: '#example-2',
+  data: {
+    checked: false
+  }
+})
+
+new Vue({
+  el: '#example-3',
+  data: {
+    checkedNames: []
+  }
+})
+
+new Vue({
+  el: '#example-4',
+  data: {
+    picked: ''
+  }
+})
+
+new Vue({
+  el: '#example-5',
+  data: {
+    selected: ''
+  }
+})
