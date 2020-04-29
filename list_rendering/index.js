b7590fadfaa4b49789e3cc59bb7bb377a0b48a4d
Wataru Ono <wtr0414@gmail.com>
2020-04-29 15:19:59 +0900

    Begin Event Handling

---
+++ list_rendering/index.js
@@ -0,0 +1,57 @@
+var example1 = new Vue({
+  el: '#example-1',
+  data: {
+    items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]
+  }
+})
+
+example1.items = example1.items.filter(function (item) {
+  return item.message.match(/Foo/)
+})
+
+var example2 = new Vue({
+  el: '#example-2',
+  data: {
+    parentMessage: 'Parent',
+    items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]
+  }
+})
+
+new Vue({
+  el: '#v-for-object',
+  data: {
+    object: {
+      title: 'How to do lists in Vue',
+      author: 'Jane Doe',
+      publishedAt: '2016-04-10'
+    }
+  }
+})
+
+new Vue({
+  el: '#app1',
+  data: {
+    numbers: [ 1, 2, 3, 4, 5 ]
+  },
+  computed: {
+    evenNumbers: function () {
+      return this.numbers.filter(function (number) {
+        return number % 2 === 0
+      })
+    }
+  },
+})
+
+new Vue({
+  el: '#app2'
+})
+
+new Vue({
+  el: '#app3'
+})
